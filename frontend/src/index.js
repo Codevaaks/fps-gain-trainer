33e865e2f983d6a1f78b97581273a3eb2de781dd
Dyanni <dyanniham@gmail.com>
2020-12-18 13:36:36 -0700

    create simple UI with three buttons that routes to multiple pages

---
+++ frontend/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch,Link} from 'react-router-dom';
+import Page1 from './routes/page1';
+import Page2 from './routes/page2';
+import Page3 from './routes/page3';
+import './index.css';
+
+
+function App(){
+    return (
+        <div>
+            <main>
+                <section>
+                    <Link to="/page1">
+                        <button type="button" className="btn">
+                            page 1
+                        </button>
+                    </Link>
+                    <Link to="/page2">
+                        <button type="button" className="btn">
+                            page 2
+                        </button>
+                    </Link>
+                    <Link to="/page3">
+                        <button type="button" className="btn">
+                            page3
+                        </button>
+                    </Link>
+                </section>
+                <Switch>
+                    <Route exact path="/page1">
+                        <Page1/>
+                    </Route>
+                    <Route path="/page2">
+                        <Page2 />
+                    </Route>
+                    <Route path="/page3">
+                        <Page3 />
+                    </Route>
+                </Switch>
+            </main>
+        </div>
+    )
+}
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
---
+++ frontend/src/routes/page1.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Page1 extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Hello, welcome to page 1</h1>
+                <h2>This is page 1</h2>
+            </div>
+        )
+    }
+}
+
+export default Page1;
